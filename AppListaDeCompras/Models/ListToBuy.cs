f8f1e3ac6fdd0742ef0758c5864c116029b75951
fillipe.felix <felipesoares_1993@hotmail.com>
2024-03-24 17:12:32 -0300

    Customizando a tela de ListToBuyPage.xaml e criando dados fake para preencher a CollectionView

---
+++ AppListaDeCompras/Models/ListToBuy.cs
@@ -0,0 +1,12 @@
+﻿namespace AppListaDeCompras.Models;
+
+public class ListToBuy
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public IList<Product> Products { get; set; } = new List<Product>();
+    public IList<User> Users { get; set; } = new List<User>();
+    
+    public DateTimeOffset CreatedAt { get; set; }
+}
---
+++ AppListaDeCompras/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace AppListaDeCompras.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    
+    //TODO - trocar para um ENUM
+    public string QuantityUnitMeasure { get; set; }
+    public decimal Price { get; set; }
+}
