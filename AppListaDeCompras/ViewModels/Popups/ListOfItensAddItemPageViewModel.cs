ff99dd37f25a224cc6d5c88fadb69afa78387c2f
fillipe.felix <felipesoares_1993@hotmail.com>
2024-03-29 16:59:21 -0300

    Correção da edição para que não fique atualizando no banco a cada interação e sim apenas quando clicar em salvar.

--- AppListaDeCompras/ViewModels/Popups/ListOfItensAddItemPageViewModel.cs
+++ AppListaDeCompras/ViewModels/Popups/ListOfItensAddItemPageViewModel.cs
@@ -13,7 +13,26 @@ namespace AppListaDeCompras.ViewModels.Popups;
 
 public partial class ListOfItensAddItemPageViewModel : ObservableObject
 {
-    [ObservableProperty] public Product? product;
+    private Product? _product;
+    public Product? Product
+    {
+        get { return _product;}
+        set { 
+            _product = value;
+            ProductForm = new Product
+            {
+                Id = value.Id,
+                Name = value.Name,
+                Price = value.Price,
+                Quantity = value.Quantity,
+                QuantityUnitMeasure = value.QuantityUnitMeasure,
+                HasCaught = value.HasCaught,
+                CreatedAt = value.CreatedAt
+            };
+        }
+    }
+    
+    [ObservableProperty] public Product? productForm;
 
     [ObservableProperty]
     private string[] unitsMeasure;
@@ -24,6 +43,7 @@ public partial class ListOfItensAddItemPageViewModel : ObservableObject
     {
         unitsMeasure = Enum.GetNames(typeof(UnitMeasure));
         Product = new Product();
+        ProductForm = new Product();
     }
     
     [RelayCommand]
@@ -37,22 +57,24 @@ public partial class ListOfItensAddItemPageViewModel : ObservableObject
     {
         //TODO - Validar os dados
         
-        //TODO - Salvar as informações
         var realm = MongoDbAtlasService.GetMainThreadRealm();
 
         await realm.WriteAsync(() =>
         {
             if (Product!.Id == default)
             {
-                Product.Id = ObjectId.GenerateNewId();
-                Product.CreatedAt = DateTime.UtcNow;
+                ProductForm.Id = ObjectId.GenerateNewId();
+                ProductForm.CreatedAt = DateTime.UtcNow;
+                
                 List.Products.Add(Product);
                 realm.Add(List, true);
             }
             else
             {
-                //TODO - Update
-                realm.Add(List, true);
+                Product.Name = ProductForm.Name;
+                Product.Quantity = ProductForm.Quantity;
+                Product.Price = ProductForm.Price;
+                Product.QuantityUnitMeasure = ProductForm.QuantityUnitMeasure;
             }
         });
         
