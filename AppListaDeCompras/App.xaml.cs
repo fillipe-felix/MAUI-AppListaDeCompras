01cfd3d600253bb0e093eba2374bc22f64f36923
fillipe.felix <felipesoares_1993@hotmail.com>
2024-03-30 20:17:10 -0300

    Ajustes finais para ajustar as bordar do entry e picker. tbm criado novo style para bordas

--- AppListaDeCompras/App.xaml.cs
+++ AppListaDeCompras/App.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using AppListaDeCompras.Views;
 
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#endif
+
 namespace AppListaDeCompras;
 
 public partial class App : Application
@@ -7,6 +11,7 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        RemoveBorders();
 
         //Mostra essa tela apenas 1x
         if (VersionTracking.IsFirstLaunchEver)
@@ -17,5 +22,24 @@ public partial class App : Application
         {
             MainPage = new AppShell();
         }
+    }
+
+    public void RemoveBorders()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("Borderless",(handler, view) => {
+#if ANDROID
+    handler.PlatformView.Background = null;
+    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#endif
+        });
+
+        Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("Borderless", (handler, view) => {
+#if ANDROID
+    handler.PlatformView.Background = null;
+    handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+    handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+#endif
+        });
     }
 }
