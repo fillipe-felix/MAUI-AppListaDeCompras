66c9d7ec7906a474dc2f03286a2939865fc7d241
fillipe.felix <felipesoares_1993@hotmail.com>
2024-03-30 18:22:52 -0300

    alteração para poder compartilhar a lista de comprar com outra pessoa.

--- AppListaDeCompras/ViewModels/Popups/ListToBuySharedPageViewModel.cs
+++ AppListaDeCompras/ViewModels/Popups/ListToBuySharedPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using AppListaDeCompras.Models;
+﻿using AppListaDeCompras.Libraries.Services;
+using AppListaDeCompras.Models;
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -9,11 +10,38 @@ namespace AppListaDeCompras.ViewModels.Popups;
 
 public partial class ListToBuySharedPageViewModel : ObservableObject
 {
-    [ObservableProperty] private ListToBuy _listToBuy /*= new ListToBuy { Users = new List<User>() }*/;
+    [ObservableProperty] private string? _email;
+    
+    [ObservableProperty] private ListToBuy _listToBuy = new ListToBuy();
 
     [RelayCommand]
     private void Close()
     {
         MopupService.Instance.PopAsync();
+    }
+
+    [RelayCommand]
+    private void Add()
+    {
+        if (string.IsNullOrWhiteSpace(Email))
+        {
+            App.Current.MainPage.DisplayAlert("Erro", "Preencha o campo e-mail!", "Ok");
+            return;
+        }
+
+        var realm = MongoDbAtlasService.GetMainThreadRealm();
+        var user = realm.All<User>()
+            .FirstOrDefault(e => e.Email.Equals(Email, StringComparison.OrdinalIgnoreCase));
+
+        if (user is null)
+        {
+            App.Current.MainPage.DisplayAlert("Não localizado", "Usuário não localizado com o e-mail informado!", "Ok");
+            return;
+        }
+
+        realm.WriteAsync(() =>
+        {
+            ListToBuy.Users.Add(user);
+        });
     }
 }
